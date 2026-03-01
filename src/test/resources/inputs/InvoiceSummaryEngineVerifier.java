a03dc9d954c8baae8559ec73cd9a446bbeae0fc4
Dino Stark <15190304050@163.com>
2026-03-01 21:23:40 +0800

    Make Example24/25 smarter with stricter ReAct tool prompts

---
+++ src/test/resources/inputs/InvoiceSummaryEngineVerifier.java
@@ -0,0 +1,17 @@
+public class InvoiceSummaryEngineVerifier {
+    private static boolean approx(double value, double expected) {
+        return Math.abs(value - expected) < 0.0001;
+    }
+
+    public static void main(String[] args) {
+        double caseA = InvoiceSummaryEngine.calculateTotal(new double[] {20.0, 30.0, 50.0}, "food", true);
+        double caseB = InvoiceSummaryEngine.calculateTotal(new double[] {10.0, 40.0}, "book", false);
+        boolean okA = approx(caseA, 102.6);
+        boolean okB = approx(caseB, 52.0);
+        if (okA && okB) {
+            System.out.println("BEHAVIOR_OK caseA=" + caseA + " caseB=" + caseB);
+        } else {
+            System.out.println("BEHAVIOR_FAIL caseA=" + caseA + " caseB=" + caseB);
+        }
+    }
+}
