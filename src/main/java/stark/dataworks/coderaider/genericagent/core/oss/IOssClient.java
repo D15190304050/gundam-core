3661e81debb99ef6af69ede08af34c8ae82dc3bd
DinoStark <15190304050@163.com>
2026-03-23 23:26:40 +0800

    Add some comment.

--- src/main/java/stark/dataworks/coderaider/genericagent/core/oss/IOssClient.java
+++ src/main/java/stark/dataworks/coderaider/genericagent/core/oss/IOssClient.java
@@ -5,6 +5,14 @@ package stark.dataworks.coderaider.genericagent.core.oss;
  */
 public interface IOssClient
 {
+    /**
+     * Puts an object into the object storage.
+     *
+     * @param key          object key.
+     * @param payload      object payload.
+     * @param contentType  object content type.
+     * @return Public URL of the object.
+     */
     String putObject(String key, byte[] payload, String contentType);
 
     String getPublicUrl(String key);
